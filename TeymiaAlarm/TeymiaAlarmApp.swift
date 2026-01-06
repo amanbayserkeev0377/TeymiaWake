968aef885be76297845fa0360ccc90b4bfb87ec9
amanbayserkeev0377 <amanbayserkeev0377@gmail.com>
2026-01-06 12:18:04 +0600

    Initial Commit

---
+++ TeymiaAlarm/TeymiaAlarmApp.swift
@@ -0,0 +1,17 @@
+//
+//  TeymiaAlarmApp.swift
+//  TeymiaAlarm
+//
+//  Created by Aman Bayserkeev on 6/1/26.
+//
+
+import SwiftUI
+
+@main
+struct TeymiaAlarmApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
